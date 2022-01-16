4b1099881dfaba473c0aa3931413d2b0d595fb51
Snow Studios <donovanlightnason@gmail.com>
2022-01-16 02:20:24 +0200

    DataBase & Tables for Staff_Hire Form

---
+++ DataBase/Sql/CreateTable(Staff_Hire [script] ).sql
@@ -0,0 +1,132 @@
+﻿
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "Forms"
+:setvar DefaultFilePrefix "Forms"
+:setvar DefaultDataPath "C:\Users\tannie\Desktop\Database\"
+:setvar DefaultLogPath "C:\Users\tannie\Desktop\Database\"
+
+GO
+:on error exit
+GO
+/*
+Detect SQLCMD mode and disable script execution if SQLCMD mode is not supported.
+To re-enable the script after enabling SQLCMD mode, execute the following:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'SQLCMD mode must be enabled to successfully execute this script.';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [$(DatabaseName)];
+
+
+GO
+
+IF (SELECT OBJECT_ID('tempdb..#tmpErrors')) IS NOT NULL DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+GO
+BEGIN TRANSACTION
+GO
+PRINT N'Creating Table [dbo].[StaffHire]...';
+
+
+GO
+CREATE TABLE [dbo].[StaffHire] (
+    [Id]                  INT           NOT NULL,
+    [UniqueID]            VARCHAR (125) NOT NULL,
+    [Name]                VARCHAR (50)  NOT NULL,
+    [Midlename]           VARCHAR (50)  NULL,
+    [Surname]             VARCHAR (45)  NOT NULL,
+    [DateOfBirth]         VARCHAR (50)  NOT NULL,
+    [Sex]                 VARCHAR (50)  NOT NULL,
+    [Position]            VARCHAR (50)  NOT NULL,
+    [Starting Salary]     INT           NOT NULL,
+    [Contract Period]     VARCHAR (50)  NOT NULL,
+    [Assigned Supervisor] VARCHAR (50)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+IF @@ERROR <> 0
+   AND @@TRANCOUNT > 0
+    BEGIN
+        ROLLBACK;
+    END
+
+IF OBJECT_ID(N'tempdb..#tmpErrors') IS NULL
+    CREATE TABLE [#tmpErrors] (
+        Error INT
+    );
+
+IF @@TRANCOUNT = 0
+    BEGIN
+        INSERT  INTO #tmpErrors (Error)
+        VALUES                 (1);
+        BEGIN TRANSACTION;
+    END
+
+
+GO
+PRINT N'Creating Index [dbo].[StaffHire].[Core]...';
+
+
+GO
+CREATE NONCLUSTERED INDEX [Core]
+    ON [dbo].[StaffHire]([UniqueID] ASC);
+
+
+GO
+IF @@ERROR <> 0
+   AND @@TRANCOUNT > 0
+    BEGIN
+        ROLLBACK;
+    END
+
+IF OBJECT_ID(N'tempdb..#tmpErrors') IS NULL
+    CREATE TABLE [#tmpErrors] (
+        Error INT
+    );
+
+IF @@TRANCOUNT = 0
+    BEGIN
+        INSERT  INTO #tmpErrors (Error)
+        VALUES                 (1);
+        BEGIN TRANSACTION;
+    END
+
+
+GO
+
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT N'The transacted portion of the database update succeeded.'
+COMMIT TRANSACTION
+END
+ELSE PRINT N'The transacted portion of the database update failed.'
+GO
+IF (SELECT OBJECT_ID('tempdb..#tmpErrors')) IS NOT NULL DROP TABLE #tmpErrors
+GO
+GO
+PRINT N'Update complete.';
+
+
+GO
---
+++ DataBase/Sql/CreateTable(Staff_Hire).sql
@@ -0,0 +1,17 @@
+﻿
+/* Create's a Table (Staff_Hire) In Database (Form) */
+
+CREATE TABLE  [Forms].[StaffHire]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[UniqueID] varchar(125) NOT NULL,
+	[Name] varchar(50) NOT NULL,
+	[Midlename] varchar(50),
+	[Surname] varchar(45) NOT NULL,
+	[DateOfBirth] varchar(50) NOT NULL,
+	[Sex] varchar(50) NOT NULL,
+	[Position] varchar(50) NOT NULL,
+	[Starting Salary] INT(45) NOT NULL,
+	[Contract Period] varchar(50) NOT NULL,
+	[Assigned Supervisor] varchar(50)
+);
